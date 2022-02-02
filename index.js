380697d4e4e1666b19e716949023fe4c221f720a
Varaprasad Reddy <Varaprasad.reddy@iprotechs.com>
2022-02-02 10:35:43 +0530

    This is the new updates from MacBook

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 import cors from "cors";
 import router from "./router.js";
 import sequelize from "./uitility/db.js";
+import path from "path";
 dotenv.config();
 const port = parseInt(process.env.PORT) || 3000;
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 //User routes from router js file
 app.use("/", cors(), router);
 app.get("/welcome", (req, res) => {
--- uitility/db.js
+++ uitility/db.js
@@ -2,9 +2,12 @@
 import Sequelize from "sequelize";
 import dotenv from "dotenv";
 import fs from "fs";
+import path from "path";
+import _ from "lodash";
 import UserModel from "../src/models/users.model.js";
 import PostModel from "../src/models/posts.model.js";
 dotenv.config();
+const __dirname = path.resolve();
 //sequelize connection setup for mariadb
 const sequelize = new Sequelize(
   process.env.DB_NAME || "insta_db",
@@ -26,17 +29,22 @@ const sequelize = new Sequelize(
 UserModel.init(sequelize);
 PostModel.init(sequelize);
 
-// Load all models
-await fs.readdirAsync("./src/models").map((fileName) => {
-  // ?? With ES6 should I System.import('./models/' + fileName)?
-  let model = require("./models/" + fileName);
-  /* NEW es6 style...? */
-  model.init(sequelize);
+const modelsDir = path.normalize(`${__dirname}/src/models`);
+console.log(modelsDir);
+fs.readdirSync(modelsDir)
+  .filter((file) => file.indexOf(".") !== 0 && file.indexOf(".map") === -1)
+  // import model files and save model names
+  .forEach((file) => {
+    console.info(`Loading model file ${file}`);
+    const model = sequelize.import(path.join(modelsDir, file));
+    db[model.name] = model;
+  });
+
+// calling all the associate function, in order to make the association between the models
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
-// associate
-// const models = sequelize.models;
-// _.map(Object.keys(models), (n) => models[n])
-//   .filter((m) => m.associate !== undefined)
-//   .forEach((m) => m.associate(models));
 export default sequelize;
